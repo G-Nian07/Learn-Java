438776391517859613884d70316f252f08321368
忌念 <16213358+G-Nian@user.noreply.gitee.com>
2025-09-12 02:23:30 +0000

    java学习
    Signed-off-by: 忌念 <16213358+G-Nian@user.noreply.gitee.com>

---
+++ ValueDemo1.java
@@ -0,0 +1,27 @@
+public class ValueDemo1{
+    public static void main(String[] args){
+        //目标：掌握常见的数据在代码中如何书写
+
+        //整数
+        System.out.println(666);
+        System.out.println(-777);
+
+        //小数
+        System.out.println(1.70);
+        System.out.println(-3.14);
+
+        //字符串
+        System.out.println("钟彦");
+
+        //字符
+        System.out.println("男");
+
+        //布尔true or false
+        System.out.println(true);
+
+        //空
+        //细节；null不能直接打印
+        //如果要用null只能用字符串的形式打印
+        System.out.println("null");
+    }
+}
