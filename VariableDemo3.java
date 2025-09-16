9588216249c1bf33d8b8aede5c8d71ca6402b205
忌念 <16213358+G-Nian@user.noreply.gitee.com>
2025-09-16 02:54:47 +0000

    java学习
    Signed-off-by: 忌念 <16213358+G-Nian@user.noreply.gitee.com>

---
+++ VariableDemo3.java
@@ -0,0 +1,35 @@
+public class  VariableDemo3{
+    //主入口
+    public static void main(String [] args){
+        //byte
+        byte b = 10;
+        System.out.println(b);
+        //short
+        short c = 20;
+        System.out.println(c);
+        //int
+        int d = 30;
+        System.out.println(d);
+        //long
+        //要定义long类型在数字后面加“L”
+        long e = 9999999999L;
+        System.out.println(e);
+
+        //float
+        //数据值后加”F“
+        float f = 10.1F;
+        System.out.println(f);
+
+        //double
+        double g = 20.2;
+        System.out.println(g);
+
+        //char
+        char y = '彦';
+        System.out.println(y);
+
+        //boolean
+        boolean h = true;
+        System.out.println(h);
+    }
+}
